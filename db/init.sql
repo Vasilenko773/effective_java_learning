64bf23e6b9a6f33275a9ae0bcb834aebc4a50279
Василенко Илья <ivvasilen1@mts.ru>
2024-07-10 16:47:17 +0300

    feat: оптимизация производительности

--- db/init.sql
+++ db/init.sql
@@ -10,3 +10,7 @@ CREATE table book
     name      VARCHAR,
     author_id INT REFERENCES author (id)
 );
+
+-- Установка индекса
+CREATE EXTENSION pg_trgm;
+CREATE INDEX inx_book_name ON book USING GIN (name gin_trgm_ops);
--- src/main/java/mr/green/learning/db/BookJpaRepository.java
+++ src/main/java/mr/green/learning/db/BookJpaRepository.java
@@ -1,10 +1,12 @@
 package mr.green.learning.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookJpaRepository extends JpaRepository<Book, Integer> {
 
+    @Query(nativeQuery = true, value = "SELECT * FROM book WHERE book.name LIKE %:say%")
     List<Book> getAllByNameContains(String say);
 }
